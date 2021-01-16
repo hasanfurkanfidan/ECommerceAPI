1a48626fc951e968e2cdfa62acff18c344c1b146
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-01-16 22:41:10 +0300

    İlerleme sağlandı

---
+++ ECommerceApi.Data/DataContext/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceApi.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceApi.Data.DataContext
+{
+    public class StoreContext:DbContext
+    {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-3VB3SSC\\SQLEXPRESS01;Database=ECommerceApi;Integrated Security=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ECommerceApi.Data/DbModels/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceApi.Data.DbModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
